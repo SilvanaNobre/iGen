a5f7c05b688bd705a0fd258047f5c3302d447504
Mauro Assis <assismauro@hotmail.com>
2022-04-28 07:26:13 -0300

    Add command line processing to iGen

--- iGen.py
+++ iGen.py
@@ -8,10 +8,20 @@ import ReadInit
 import WriteDB as wdb
 from InferenceEngine import BuildATree
 from ReadInit import InitVar
+import argparse
+
+def ProcessCmdLine():
+    parser = argparse.ArgumentParser(description='iGen, alternatives of forestry management generator.')
+    parser.add_argument('-pp', '--projectpath', help='Path where project is stored or will be created')
+    parser.add_argument('-pt', '--projecttemplatepath', help='Project template to be cloned to project path', type=str)
+    return parser.parse_args()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # read variables to Initialization
+    args = ProcessCmdLine()
+    print(args.projectpath)
+    print(args.projecttemplatepath)
     ReadInit.GetInit('RomeroInitData.json')
 
     # Init.DbFile comes from initialization variables read in ReadInit.GetInit
