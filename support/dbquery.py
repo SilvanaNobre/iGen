d8d6863bbf6d1cb21212a5c953bb510caae57d84
Mauro Assis <assismauro@hotmail.com>
2022-07-06 21:44:25 -0300

    Update .gitignore

--- support/dbquery.py
+++ support/dbquery.py
@@ -1,14 +1,20 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 import pandas as pd
 import support.app_object as app_object_support
 
 db = None
+
+engine = None
+
+session = None
 
 def getEngine(uri: str):
     return create_engine(uri)
 
 
 def connectDB():
+    global engine
     engine = getEngine(db)
     return engine.connect()
 
@@ -16,7 +22,12 @@ def connectDB():
 def executeSQL(sql):
     conn = connectDB()
     try:
-        return conn.execute(sql).fetchall() if 'SELECT ' in sql.upper() else None
+        if 'SELECT ' in sql.upper():
+            return conn.execute(sql).fetchall()
+        else:
+            with Session(engine) as session:
+                engine.execute(sql)
+                session.commit()
     except Exception as e:
         raise e
 
@@ -38,8 +49,9 @@ def getJSONResultset(sql):
 def getDataframeResultSet(sql):
     return pd.read_sql(sql, connectDB())
 
-def executeMany(sql: str, args_list: list):
+def executeMany(sql_mask: str, args_list: list):
     for args in args_list:
+        sql = sql_mask
         for arg in args:
             sql = sql.replace('?', f"'{arg}'", 1)
         executeSQL(sql)
